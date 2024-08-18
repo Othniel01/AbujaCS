38cb513f5a59d184aa6b06814b438f1e8414efb7
Othniel Abalaka <othnielabalaka@gmail.com>
2024-08-18 12:22:57 +0100

    new changes

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -21,7 +21,7 @@ export default function AboutSection() {
         </p>
 
         <div className="aboutFlex">
-          <video className="newVideo" controls>
+          <video className="newVideo" controls="true">
             <source src={videoSource} type="video/mp4" />
             Your browser does not support the video tag.
           </video>
